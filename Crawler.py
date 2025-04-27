f778f049e8531e98856f776d9d0e64a17e7d88a4
Nathan <148985551+nathan-004@users.noreply.github.com>
2025-04-27 22:14:54 +0200

    Update Crawler.py

--- Crawler.py
+++ Crawler.py
@@ -1,16 +1,21 @@
 from web import WebScrapping, Stack
+from stockage import JSONStockage, TXTStockage
+
 
 class Crawler():
     def __init__(self, start_url):
         self.url = start_url
         self.web = WebScrapping(url=self.url)
+        self.stack = Stack()  # Déplacer la pile ici
 
     def crawl_dfs(self):
         """
         Parcours en profondeur (DFS) du site web à partir de l'URL de départ.
         """
         stack = Stack()
-        stack.push(self.url)
+        stack.string_to_stack(TXTStockage("urls_stack.txt").load())
+        if stack.is_empty():
+            stack.push(self.url)
         visited = set()
 
         while not stack.is_empty():
@@ -45,18 +50,23 @@ class Crawler():
         """
         Parcours en largeur (BFS) du site web à partir de l'URL de départ.
         """
-        queue = Stack()
-        queue.push(self.url)
+        self.stack.string_to_stack(TXTStockage("urls_stack.txt").load())
+        if self.stack.is_empty():
+            self.stack.push(self.url)
+        self.stack.push(self.url)  # Utiliser self.stack
         visited = set()
+        self.json_stockage = JSONStockage("urls.json")
 
-        while not queue.is_empty():
-            url = queue.pop(0)
+        while not self.stack.is_empty():
+            url = self.stack.pop(0)  # Utiliser self.stack
             print("Visiting:", url)
             if url not in visited:
                 visited.add(url)
                 self.web.url = url
                 self.web.elements = self.web.get_html_elements()
                 balises = self.web.find_balise("a")
+                valid_urls = []
+
                 for balise in balises:
                     if not "href" in balise[1]:
                         continue
@@ -76,21 +86,25 @@ class Crawler():
                         continue
                     
                     new_url = self.web.urljoin(url, balise[1]["href"])
-                    if new_url not in visited and self.web.is_valid_url(new_url) and new_url not in queue.stack:
-                        queue.push(new_url)
-                        #print(len(queue.stack),new_url, sep=" | ")
-                    else:
-                        #print("URL déjà visitée ou invalide:", new_url)
-                        pass
-            else:
-                #print("URL déjà visitée:", url)
-                pass
-        print(queue.stack)
+                    if new_url not in visited and self.web.is_valid_url(new_url) and new_url not in self.stack.stack:
+                        self.stack.push(new_url)  # Utiliser self.stack
+                    valid_urls.append(new_url)
+            
+                # Ajouter les données à sauvegarder
+                self.json_stockage.data_append(url, valid_urls, self.web.elements)
 
 if __name__ == "__main__":
     start_url = "https://webscraper.io/test-sites"
     crawler = Crawler(start_url)
-    crawler.crawl_bfs()
+    try:
+        crawler.crawl_bfs()
+    except KeyboardInterrupt:
+        text_stockage = TXTStockage("urls_stack.txt")
+        print("Sauvegarde de la pile dans le fichier urls_stack.txt")
+        text_stockage.save(crawler.stack.stack_to_string())
+        print("Sauvegarde des données dans le fichier urls.json")
+        crawler.json_stockage.save_stock()
+
 
 # https://webscraper.io/test-sites
 # https://www.w3schools.com/html/tryit.asp?filename=tryhtml_intro
