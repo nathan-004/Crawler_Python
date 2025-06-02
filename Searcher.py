c04d2b281cba26b3fe9e82b105e9c791fc6cc7ec
Nathan <148985551+nathan-004@users.noreply.github.com>
2025-06-02 14:30:10 +0200

    Ajout du scoring d'une page avec l'input
    - Cette formule : https://media.geeksforgeeks.org/wp-content/uploads/20240124120507/TF-IDF.webp

--- Searcher.py
+++ Searcher.py
@@ -1,3 +1,6 @@
+import math
+import sqlite3
+import json
 
 class Searcher:
     """Find the most relevant urls from an input"""
@@ -14,3 +17,34 @@ class Searcher:
         query:str
         """
         words = query.split(" ")
+        
+        pass
+        
+    def keyword_searching_algorithm(self, word, url):
+        """Use words.db to assign a score to an url with the word"""
+        conn = sqlite3.connect("words.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM words WHERE word=?", (word,))
+        line = cursor.fetchone()
+        conn.close()
+        
+        if line is None:
+            return 0
+        else:
+            urls = json.loads(line[2])
+            if not url in urls:
+                return 0
+            else:
+                tf = urls[url]
+                df = len(urls)
+        conn = sqlite3.connect("urls.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM urls")
+        N = len(cursor.fetchall())
+        
+        conn.close()
+        return tf * math.log(N/df)
+    
+if __name__ == "__main__":
+    s = Searcher()
+    print(s.keyword_searching_algorithm("le", "https://fr.wikipedia.org/wiki/Test"))
