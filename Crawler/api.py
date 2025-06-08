c505a5e83c363b1fcae1819e28289ec3fff9733a
Nathan <148985551+nathan-004@users.noreply.github.com>
2025-06-08 23:08:04 +0200

    Serveur python

--- Crawler/api.py
+++ Crawler/api.py
@@ -1,8 +1,13 @@
 from Searcher import Searcher
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.rout("/")
-def get_results():
-    pass
+@app.route("/search")
+def search():
+    query = request.args.get("q")
+    print("Requête reçue :", query)
+    return jsonify({"Value": 0})
+
+if __name__ == "__main__":
+    app.run()
