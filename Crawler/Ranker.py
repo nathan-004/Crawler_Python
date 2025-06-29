f64ccae0ba9d6764f8e918a08c5f1f01a4000ec1
Nathan <148985551+nathan-004@users.noreply.github.com>
2025-06-29 16:51:05 +0200

    feat: création du graphe pour l'éval. des pages

--- Crawler/Ranker.py
+++ Crawler/Ranker.py
@@ -1,37 +1,45 @@
+from stockage import SQLStockage
+from web import WebScrapping as ws
+from display import ColorDisplay
 
-def graph():
-    pass
+import sqlite3
+import json
+from random import random
 
-class Node():
-    """Noeud contenant le poids, les liens, ..."""
-    
-    def __init__(self, url, links:list):
-        """
-        Parameters
-        ----------
-        url:str
-        links:list
-            Listes des liens sur la page
-        """
+URLS = SQLStockage("urls.db").get_urls()
+N = len(URLS)
+console = ColorDisplay(debug=False)
 
-class Graph:
-    """Graphe contenant toutes les pages"""
-    
-    def __init__(self, pages:list):
-        """
-        Parameters
-        ----------
-        pages:list
-            Liste des pages existantes
-        """
-        
-        self.pages = pages
-        
-        conn = sqlite3.connect("words.db")
+
+class Node:
+    """Représente une page"""
+
+    def __init__(self, name, N):
+        self.name = name
+        conn = sqlite3.connect("urls.db")
         cursor = conn.cursor()
-        self.nodes = []
-        for url in pages:
-            cursor.execute("SELECT ")
+        cursor.execute("SELECT links FROM urls WHERE url = ?", (name,))
+        self.links = []
+        for balise in json.loads(cursor.fetchone()[0]): # ["a", {"href": "Lien relatif"}, "Contenu du lien"]
+            try:
+                self.links.append(ws.urljoin(ws, name, balise[1]["href"]))
+            except Exception as e:
+                console.warning(e)
         conn.close()
+
+        self.value = 1/N
+
+    def distribute(self, graph, d):
+        """
+        Distribue la valeur de PageRank vers les liens de cette page
+        :d float: nombre entre 0 et 1 -> probabilité que l'utilisateur continue sur un lien de la page
+        """
+        pass
+
+if __name__ == "__main__":
+    console.log(f"Création du graphe | {len(URLS)} urls")
+    graph = {url: Node(url, N) for url in URLS}
+    console.quit()
+    console.validate("Graphe terminé")
 
 # https://web.stanford.edu/class/cs54n/handouts/24-GooglePageRankAlgorithm.pdf
